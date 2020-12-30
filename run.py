14e867145c1a3d482726045b236b4ea094f56d0e
natppu <natppu@OUCL12554.local>
2020-12-30 14:15:53 +0000

    update run.py

--- run.py
+++ run.py
@@ -11,7 +11,20 @@ def get_ind(file):
 		return -1
 	return int(ind[0])
 
-workbook = xlsxwriter.Workbook('result.xlsx') 
+def cvc4_process(root, file):
+	return subprocess.Popen('cvc4 ' + 
+		os.path.join(root,file), stdout=subprocess.PIPE, 
+		stderr=subprocess.PIPE, shell=True)
+
+def cvc4_post_process(output):
+	if 'unsat' not in str(output):
+		print(colored("[WARNING] FAILED",'magenta'))
+		return False, 'FAILED'
+	output = str(output).replace('b\'unsat\\n','').replace('\'','').replace('\\n','')
+	print(output)
+	return True, output
+
+workbook = xlsxwriter.Workbook('result1.xlsx') 
 worksheet = workbook.add_worksheet() 
 
 worksheet.write('A1', 'Folder') 
@@ -35,7 +48,7 @@ for root, dir, files in os.walk("."):
 		worksheet.write('B' + str(i), file)
 
 		start_time = time.time()
-		p = subprocess.Popen('cvc4 ' + os.path.join(root,file), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True) 
+		p = cvc4_process(root, file)
 		try:
 			output, o_err = p.communicate(timeout=60)
 			p.kill()
@@ -51,21 +64,18 @@ for root, dir, files in os.walk("."):
 			worksheet.write('C' + str(i), 'TIMEOUT')
 			i = i + 1
 			continue
-
-		if 'unsat' not in str(output):
-			print(colored("[WARNING] FAILED",'magenta'))
-			worksheet.write('D' + str(i), 'FAILED')
-			i = i + 1
-			continue
-
 		end_time = time.time()
-		output = str(output).replace('b\'unsat\\n','').replace('\'','').replace('\\n','')
-		print(output)
+
+		flag, output = cvc4_post_process(output)
+		if not flag:
+			worksheet.write('D' + str(i), 'FAILED')
+		else:
+			worksheet.write('D' + str(i), output)
+
 		worksheet.write('C' + str(i), end_time - start_time)
-		worksheet.write('D' + str(i), output)
 
 		i = i+1
 	i = i+1
-
 	print ("")
+
 workbook.close()
